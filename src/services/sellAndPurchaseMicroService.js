8f20ccbdc56489293b1911ad6681cbf1949a2481
Srinivas Relangi <srinivas@leewayhertz.com>
2022-02-15 17:15:24 +0530

    integrate top-collection

--- src/services/sellAndPurchaseMicroService.js
+++ src/services/sellAndPurchaseMicroService.js
@@ -23,3 +23,25 @@ export function getTopSellers(requestData) {
             return Promise.reject(err);
         });
 }
+export function getTopCollections(requestData) {
+    let url = process.env.REACT_APP_SELL_AND_PURCHASE_MICROSERVICE + "api/v1/top-collections";
+    return httpService(
+        httpConstants.METHOD_TYPE.GET,
+        { "Content-Type": httpConstants.CONTENT_TYPE.APPLICATION_JSON },
+        requestData,
+        url
+    )
+        .then((response) => {
+            if (
+                !response.success ||
+                response.responseCode !== 200 ||
+                !response.responseData ||
+                response.responseData.length === 0
+            )
+                return Promise.reject();
+            return Promise.resolve(response.responseData);
+        })
+        .catch(function (err) {
+            return Promise.reject(err);
+        });
+}
