97a0a6723df10a99a47beb87b0b3289834ca88a3
alok-parangat <alok.k@parangat.com>
2022-03-02 15:04:44 +0530

    work on add-wallet screen design and responsive

--- src/modules/Create/Create.js
+++ src/modules/Create/Create.js
@@ -114,83 +114,85 @@ function Create() {
         />
       </div>
 
-      <div className="container">
-        <div className="mt-5">
-          <h1 style={{ fontSize: "20px", fontWeight: "bolder" }}>
-            Connect your wallet
-          </h1>
-
-          <p
-            style={{ fontSize: "18px", color: "#8B8B8B", fontWeight: "normal" }}
-          >
-            Connect with one of our most popular{" "}
-            <span style={{ color: "#8B8B8B", fontWeight: "bold" }}>
-              {" "}
-              wallets
-            </span>{" "}
-            providers
-            <br />
-            or create a new one
-          </p>
-        </div>
-        <div className="row createmob">
-          <div
-            onClick={connectMetamask}
-            className="eachWalletTypeBox card col-md-3 col-lg-3 col-sm-6 col-12 my-5 card-border"
-            style={{ cursor: "pointer" }}
-          >
-            <img
-              id="create_logo"
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title"> Metamask</h5>
-              <p className="card-text">
-                One of the most secure wallets
-                <br /> with great flexibility
-              </p>
-            </div>
+      <div className="container-fluid connect-wallet">
+        <div className="row">
+          <div className="col-xl-12">
+            <h1 className="title">Connect your wallet</h1>
+            <p className="text">
+              Connect with one of our most popular{" "}
+              <span>
+                {" "}
+                wallets
+              </span>{" "}
+              providers
+              <br />
+              or create a new one
+            </p>
           </div>
-
-          {/* <div className="eachWalletTypeBox card col-md-3 col-lg-3 col-sm-6 col-12 mx-4 my-5 card-border createmob2">
-            <img
-              id="create_logo"
-              src={image}
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title">Torus</h5>
-              <p className="card-text">
-                Connect with your Google,
-                <br /> Facebook, Twitter or Discord
-              </p>
-            </div>
-          </div> */}
-
-          {/* <div className="eachWalletTypeBox card col-md-3 col-lg-3 col-sm-6 col-12 my-5 card-border">
-            <img
-              id="create_logo"
-              src="https://api.nuget.org/v3-flatcontainer/walletconnect.core/1.6.5/icon"
-              className="card-img-top"
-              alt="..."
-            />
-            <div className="card-body">
-              <h5 className="card-title">Wallet Connect</h5>
-              <p className="card-text">
-                Open protocol for connecting <br />
-                Wallets to Dapps
-              </p>
-            </div>
-          </div> */}
         </div>
-        {/* <button
-          type="button"
-          className="ShowMoreButtonConnectWallet btn btn-outline-primary btn-size createmobbtn"
-        >
-          Show more
-        </button> */}
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="eachWalletTypeBox d-flex justify-content-start flex-wrap">
+              <div
+                onClick={connectMetamask}
+                className="card"
+                style={{ cursor: "pointer" }}
+              >
+                <img
+                  id="create_logo"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/MetaMask_Fox.svg/800px-MetaMask_Fox.svg.png"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title"> Metamask</h5>
+                  <p className="card-text">
+                    One of the most secure wallets
+                    <br /> with great flexibility
+                  </p>
+                </div>
+              </div>
+
+              {/* <div className="card">
+                <img
+                  id="create_logo"
+                  src={image}
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Torus</h5>
+                  <p className="card-text">
+                    Connect with your Google,
+                    <br /> Facebook, Twitter or Discord
+                  </p>
+                </div>
+              </div>
+
+              <div className="card">
+                <img
+                  id="create_logo"
+                  src="https://api.nuget.org/v3-flatcontainer/walletconnect.core/1.6.5/icon"
+                  className="card-img-top"
+                  alt="..."
+                />
+                <div className="card-body">
+                  <h5 className="card-title">Wallet Connect</h5>
+                  <p className="card-text">
+                    Open protocol for connecting <br />
+                    Wallets to Dapps
+                  </p>
+                </div>
+              </div> */}
+            </div>
+         
+            {/* <button
+              type="button"
+              className="btn btn-outline-primary btn-size"
+            >
+              Show more
+            </button> */}
+          </div>
+        </div>
       </div>
     </>
   );
