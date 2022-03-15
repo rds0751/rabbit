46f931156001f2ac501cf1ac4d0998f93d291a7b
gautam sharma <gautamsh@leewayhertz.com>
2022-03-15 19:42:33 +0530

    top-buyer ui

--- src/modules/TopList/TopBidders.js
+++ src/modules/TopList/TopBidders.js
@@ -1,5 +1,7 @@
-import React, { Component } from "react";
-// import './Top_collection.css'
+import React, { useState, useEffect } from "react";
+import { getTopCollections } from "../../services/sellAndPurchaseMicroService";
+import styled from "styled-components";
+import dropdown from "../../assets/images/drop down.png";
 import {
   LeaderBoardApi,
   LeaderBoardApi2,
@@ -8,119 +10,133 @@ import {
   LeaderBoardApi5,
 } from "../../constants/LeaderBoardApi";
 
-export class TopBidders extends Component {
-  render() {
-    return (
-      <div
-        className="topbidders_mob"
-        style={{ marginLeft: "128PX", marginRight: "128px" }}
-      >
-        {/* <h2>Hello</h2> */}
-        <div className="container">
-          <div className="row">
-            <div className="col" style={{padding:"0"}}>
-              <div
-                className="Leader_Board_container"
-                style={{ marginBottom: "30px" }}
-              >
-                <h1 style={{ fontSize: "20px", fontWeight: "bold" }}>Top Buyers</h1>
-                <select className="top-dropdown">
-                  <option>Weekly</option>
-                  <option>Monthly</option>
-                  <option>Yearly</option>
-                </select>
-                {/* <div className="dropdown">
-                  <button
-                    className="btn btn-secondary dropdown-toggle"
-                    type="button"
-                    id="dropdownMenuButton1"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                    placeholder="All"
-                    style={{
-                      backgroundColor: "white",
-                      color: "black",
-                      border: "1px solid #ddd",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    Weekly
-                    <img src={require("../../assets/images/downarrow.png")} style={{marginLeft:"39px"}}/>
-                  </button>
-                  <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </div> */}
-              </div>
-            </div>
-          </div>
-        </div>
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 140px;
+  margin-right: 130px;
+  color: #191919;
+  margin-top: 29px;
+  @
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const Title = styled.h3`
+  font-size: 20px;
+  font-weight: 600;
+`;
+const Select = styled.select`
+  width: 118px;
+  height: 42px;
+  padding: 11px 8px 11px 8px;
+  font-size: 14px;
+  font-weight: 500;
+  border: 1px solid #707070;
+  background: #ffffff;
+  border-radius: 6px;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-image: url(${dropdown});
+  background-repeat: no-repeat;
+  background-position: 90% center;
+`;
+const Body = styled.div`
+  margin-top: 51px;
+  margin-bottom: 16px;
+`;
+const Column = styled.div`
+  font-size: 16px;
+  font-weight: 600;
+`;
+const Collection = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #f8f8f8;
+  margin-bottom: 42px;
+  height: 68px;
+`;
+const Image = styled.img`
+  width: 42px;
+  height: 42px;
+  /* padding-left: 31px;
+  padding-right: 14px; */
+  border-radius: 10px;
+  margin-left: 31px;
+  margin: auto 0;
+`;
+const NameColumn = styled.div`
+  display: flex;
+`;
+const Name = styled.p`
+  font-size: 16px;
+  font-weight: 500;
+  margin: 24px 0 21px 14px;
+`;
+const VolumeColumn = styled.div`
+  padding-left: 40px;
+  margin:auto 0;
+`;
+const Span = styled.p`
+  color: #366eef;
+  font-size: 16px;
+  font-weight: 500;
+`;
+const Text = styled.div`
+  font-size: 16px;
+  font-weight: 500;
+`;
+const Volume=styled.span`
+font:normal normal normal 16px/25px Poppins;
+color: #818181;
+`;
 
-        <div className="container CollectionHeading">
-          <div className="row">
-            <div className="col"
-            // style={{ paddingLeft: "40px" }}
-            >
-              Name
-            </div>
-            <div className="col" style={{ padding: "0px", margin: "0px" }}>
-              Volume
-            </div>
-            <div className="col" style={{ padding: "0px", margin: "0px" }}>
-              Items bought
-            </div>
-          </div>
+function Top_collection() {
+  const [topCollections, setTopCollections] = useState([]);
+
+
+  return (
+    <Container>
+      <Header>
+        <Title>Top Buyers</Title>
+        <Select>
+          <option value="weekly">Weekly</option>
+          <option value="monthly">Monthly</option>
+          <option value="yearly">Yearly</option>
+        </Select>
+      </Header>
+      <Body className="container-fluid">
+        <div className="row">
+          <Column className="col" style={{ paddingLeft: "42px" }}>
+            Name
+          </Column>
+          <Column className="col">Volume</Column>
+          <Column className="col">ItemsBought</Column>
         </div>
-        {LeaderBoardApi4.map((curElem) => {
-          const { name, volume, itemsbought } = curElem;
-          return (
-            <div className="container ">
-              <div
-                className="row CollectionItem"
-                style={{ backgroundColor: "#f8f8f8" }}
-              >
-                <div className="col">
-                  {" "}
-                  <img
-                    className="rounded-circle"
-                    src="https://earncashto.com/wp-content/uploads/2021/06/495-4952535_create-digital-profile-icon-blue-user-profile-icon.png"
-                    alt="/"
-                  />
-                  <h2>{name} </h2>
-                </div>
-                <div
-                  className="col"
-                  style={{ color: "#818181", fontWeight: "normal" }}
-                >
-                  {" "}
-                  <span style={{textAlign: "left",font: "normal normal medium 16px/25px Poppins",
-                  letterSpacing: "0px",color:"#366EEF"}}>34 ETH</span>
-                  {volume}
-                </div>
-                <div className="col">{itemsbought}</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
-  }
+      </Body>
+      {LeaderBoardApi4.map((curElem) => {
+        const { img,name, volume, itemsbought} =
+          curElem;
+        return (
+          <div className="container-fluid">
+            <Collection className="row">
+              <NameColumn className="col">
+                <Image src={img} alt="pic" />
+                <Name>{name}</Name>
+              </NameColumn>
+              <VolumeColumn className="col">
+                <Span>34 ETH  <Volume>{volume}</Volume> </Span>
+               
+              </VolumeColumn>
+              <Text className="col">{itemsbought}</Text>
+            </Collection>
+          </div>
+        );
+      })}
+    </Container>
+  );
 }
 
-export default TopBidders;
+export default Top_collection;
