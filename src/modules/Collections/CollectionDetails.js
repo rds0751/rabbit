dee36c0748734a89f40f9ecc2a15203c14cacfff
Ravindar Reddy <ravindar@leewayhertz.com>
2022-03-25 21:29:59 +0530

    filters integration on collection-details page

--- src/modules/Collections/CollectionDetails.js
+++ src/modules/Collections/CollectionDetails.js
@@ -15,33 +15,86 @@ import search from "../../assets/images/search.svg";
 import dropdown from "../../assets/images/dropdown.svg";
 import NftCardsHome from "../../common/components/NftCardsHome";
 import CollDetailCard from "../../common/components/CollDetailCard";
-import NoItem from "../../assets/images/Noitems.svg"
+import NoItem from "../../assets/images/Noitems.svg";
+import { Button } from "react-bootstrap";
+const queryString = require('query-string');
 
 function CollectionDetails() {
+  const defaultFilter = {
+    searchByName: "",
+    status: "",
+    sortBy: "",
+    minPrice: "",
+    maxPrice: "",
+  }
   const collectionId = useParams();
   const [collection, setCollection] = useState([]);
   const [statusDrop, setStatusDrop] = useState(false);
-  const [priceDrop, setpriceDrop] = useState(false);
-  const [sortDrop, setsortDrop] = useState(false);
+  const [priceDrop, setPriceDrop] = useState(false);
+  const [sortDrop, setSortDrop] = useState(false);
   const [nfts, setNfts] = useState([]);
   const [checkLike, setcheckLike] = useState(false);
-
+  const [minPrice, setMinPrice] = useState();
+  const [maxPrice, setMaxPrice] = useState();
+  const [status, setStatus] = useState("");
+  const [sort, setSort] = useState("");
+  const [filter, setFilter] = useState(defaultFilter);
+  const sortOptions = ["Recently added", "Price high to low", "Price low to high"]
+  const statusOptions = ["Open for sale", "New"]
+  
   useEffect(() => {
     async function fetchData() {
       await getCollection(collectionId.id).then((res) => {
         setCollection(res);
       });
-      await getNftsByCollectionId(collectionId.id).then((res) => {
-        setNfts(res);
-      });
     }
     fetchData();
   }, []);
 
-  const { imageUrl, coverUrl, name, description } = collection;
+  useEffect(() => {
+    async function fetchData() {
+      const reqObj = queryString.stringify(filter);
+      await getNftsByCollectionId(collectionId.id, reqObj).then((res) => {
+        setNfts(res.nftContent);
+      });
+    }
+    fetchData();
+  }, [filter]);
+
+  const { _id, imageUrl, coverUrl, name, description } = collection;
   const handleLike = () => {
     setcheckLike(!checkLike);
   };
+
+  const onSortHandle = (e) => {
+    setSort(e.target.textContent)
+    if (e.target.textContent === "Recently added") {
+      setFilter({...filter, "sort": 1})
+    } else if (e.target.textContent === "Price high to low"){
+      setFilter({...filter, "sort": 2})
+    } else {
+      setFilter({...filter, "sort": 3})
+    }
+  }
+  const onStatusHandle = (e) => {
+    setStatus(e.target.textContent)
+    if (e.target.textContent === "New") {
+      setFilter({...filter, "status": "new"})
+    } else {
+      setFilter({...filter, "status": "onsale"})
+    }
+  }
+
+  const setPrice = () => {
+    setFilter({...filter, "minPrice": minPrice, "maxPrice": maxPrice})
+    setPriceDrop(!priceDrop)
+  }
+
+  const clearValues = () => {
+    setMinPrice("")
+    setMaxPrice("")
+  }
+
   return (
     <>
       <div>
@@ -79,14 +132,15 @@ function CollectionDetails() {
         <div className="collection-body">
           <div className="collfilters">
             <div className="colleftfilter">
-              {/* <h1>Top NFT sales</h1> */}
               <div className="searchboxcol">
                 <input
                   type="text"
+                  name="searchByName"
                   placeholder="Search"
                   aria-label="Recipient's username"
                   aria-describedby="button-addon2"
                   className="inputsearch"
+                  onChange = {(e) => setFilter({...filter, "searchByName":e.target.value })}
                 />
                 <div>
                   {/* <button type="button" id="button-addon2"> */}
@@ -94,59 +148,76 @@ function CollectionDetails() {
                   {/* </button> */}
                 </div>
               </div>
-              <div
-                className="colldrop"
-                onClick={() => setStatusDrop(!statusDrop)}
-              >
-                <div className="statusText">Status</div>
-                <div>
-                  <img src={dropdown} />
+              <div className="colldrop"  onClick= {e => setStatusDrop(!statusDrop)}>
+                <div className="statusText">
+                  {status? status : "Status"}
                 </div>
-                <div
-                  className="dropitems "
-                  style={{ display: statusDrop ? "block" : "none" }}
-                >
-                  <div className="dropsingleitem">All</div>
-                  <div className="dropsingleitem">status 1</div>
-                  <div className="dropsingleitem">status 1</div>
+                <div><img src={dropdown} /></div>                
+                {statusDrop && (
+                  <div className="dropitems">
+                    {statusOptions.map((option) => (
+                      <div className="dropsingleitem" onClick={e => {onStatusHandle(e)}} >{option}</div>
+                    ))}
                 </div>
+                )}
+
               </div>
-              <div
-                className="colldrop"
-                onClick={() => setpriceDrop(!priceDrop)}
-              >
-                <div className="statusText">Price</div>
-                <div>
-                  <img src={dropdown} />
+              <div className="colldrop" style={{width:"200px"}}>
+                <div className="statusText">
+                  Price range All
                 </div>
-                <div
-                  className="dropitems"
-                  style={{ display: priceDrop ? "block" : "none" }}
-                >
-                  <div className="dropsingleitem">All</div>
-                  <div className="dropsingleitem">status 1</div>
-                  <div className="dropsingleitem">status 1</div>
+                <div><img src={dropdown} alt="arrow" onClick= {e => setPriceDrop(!priceDrop)}/></div>                
+                {priceDrop && (
+                  <div className="dropitems" style={{width:"200px"}}>
+                    <div className="row mb-3 align-items-center">
+                      <div className="col-5">
+                        <input type="number"
+                        className="form-control"
+                        placeholder="Min"
+                        name="minPrice"
+                        value={minPrice}
+                        onChange={(e) => setMinPrice(e.target.value)}
+                      />
+                      </div>
+                      <div className="col-2 text-center">
+                        <span className="to">to</span>
+                      </div>
+                      <div className="col-5">
+                        <input type="number"
+                        className="form-control"
+                        placeholder="Max"
+                        name="maxPrice"
+                        value={maxPrice}
+                        onChange={(e) => setMaxPrice(e.target.value)} />
+                      </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-6">
+                      <Button variant="outline-primary" onClick={clearValues}>Clear</Button>
+                    </div>
+                    <div className="col-6">
+                      <Button variant="outline-primary" onClick={setPrice}>Apply</Button>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <div
-                className="colldrop ms-auto"
-                onClick={() => setsortDrop(!sortDrop)}
-              >
-                <div className="statusText">Sort By</div>
-                <div>
-                  <img src={dropdown} />
+                )}
+
+              </div>            
+              <div className="colldrop ms-auto"  onClick= {e => setSortDrop(!sortDrop)}>
+                <div className="statusText">
+                  {sort? sort : "Sort By"}
                 </div>
-                <div
-                  className="dropitems "
-                  style={{ display: sortDrop ? "block" : "none" }}
-                >
-                  <div className="dropsingleitem">All</div>
-                  <div className="dropsingleitem">status 1</div>
-                  <div className="dropsingleitem">status 1</div>
+                <div><img src={dropdown} /></div>                
+                {sortDrop && (
+                  <div className="dropitems">
+                    {sortOptions.map((option) => (
+                      <div className="dropsingleitem" onClick={e => {onSortHandle(e)}} >{option}</div>
+                    ))}
                 </div>
-              </div>
-            </div>
-            
+                )}
+
+              </div>  
+              </div>        
           </div>
           <div className="nftTileContainer row cards-gap ntf_row">
 
