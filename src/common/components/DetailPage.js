ee16f8804785a0d4c810ad30d8c76d418260586b
gautam sharma <gautamsh@leewayhertz.com>
2022-05-02 19:30:53 +0530

    detailspage ui changes

--- src/common/components/DetailPage.js
+++ src/common/components/DetailPage.js
@@ -3,8 +3,6 @@ import Table from "@mui/material/Table";
 import TableContainer from "@mui/material/TableContainer";
 import styled from "styled-components";
 
-
-
 const TableDown = styled(Table)`
   background: #ffffff 0% 0% no-repeat padding-box;
   border-radius: 0px 0px 3px 3px;
@@ -38,31 +36,67 @@ export default function DetailPage(props) {
           <tbody>
             <tr sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
               <td
-                style={{border:"none"}}
+                style={{
+                  border: "none",
+                  width: "34%",
+                  font: "normal normal 600 14px/21px Poppins",
+                  paddingTop:"19px",
+                  paddingLeft:"20px",
+                }}
                 component="td"
                 scope="row"
               >
                 Contract address
               </td>
-              <td  style={{border:"none"}}>-</td>
+              <td style={{ border: "none" }}>-</td>
             </tr>
             <tr sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <td component="td" scope="row"  style={{border:"none"}}>
+              <td
+                component="td"
+                scope="row"
+                style={{
+                  border: "none",
+                  width: "34%",
+                  font: "normal normal 600 14px/21px Poppins",
+                  paddingTop:"20px",
+                  paddingLeft:"20px",
+                }}
+              >
                 Token ID
               </td>
-              <td  style={{border:"none"}}>{nft.tokenId}</td>
+              <td style={{ border: "none" }}>{nft.tokenId}</td>
             </tr>
             <tr sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <td component="td" scope="row"  style={{border:"none"}}>
+              <td
+                component="td"
+                scope="row"
+                style={{
+                  border: "none",
+                  width: "34%",
+                  font: "normal normal 600 14px/21px Poppins",
+                  paddingTop:"20px",
+                  paddingLeft:"20px",
+                }}
+              >
                 Token Standard
               </td>
-              <td style={{border:"none"}}>-</td>
+              <td style={{ border: "none" }}>-</td>
             </tr>
             <tr sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-              <td component="td" scope="row"  style={{border:"none"}}>
+              <td
+                component="td"
+                scope="row"
+                style={{
+                  border: "none",
+                  width: "34%",
+                  font: "normal normal 600 14px/21px Poppins",
+                  paddingTop:"20px",
+                  paddingLeft:"20px",
+                }}
+              >
                 Blockchain
               </td>
-              <td  style={{border:"none"}}>{nft.blockchain}</td>
+              <td style={{ border: "none" }}>{nft.blockchain}</td>
             </tr>
           </tbody>
         </TableDown>
