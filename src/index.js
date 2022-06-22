7311aea90805538a6010fab8bf1460a0639dae1a
gautam sharma <gautamsh@leewayhertz.com>
2022-06-22 17:44:40 +0530

    new build

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import { store } from "./reducers/store";
 import { Provider } from "react-redux";
 let parseData = window.location.host.split(".");
 
-if (
-  (parseData?.length === 3 && parseData[0] === "www") ||
- (parseData?.length <= 2)
-) {
+
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
@@ -19,13 +16,4 @@ if (
     </React.StrictMode>,
     document.getElementById("root")
   );
-} else {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById("root")
-  );
-}
+
